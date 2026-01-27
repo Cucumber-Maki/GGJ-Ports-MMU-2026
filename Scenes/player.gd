d4146be25250bf976f977f4e27cb8868afcc488d
jawdan-dev <34403286+jawdan-dev@users.noreply.github.com>
2026-01-27 23:21:04 +0000

    feat: player gravity on walls

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,54 +1,67 @@
 extends RigidBody2D
-@export var movementspeed := 700
+
+@export var movement_speed := 700
+
 var momentum : Vector2 = Vector2.ZERO
-var attatchedrail : WorldRailsInternal.RailPath = null
-var attatchedposition : float = 0
+var attatched_rail : WorldRailsInternal.RailPath = null
+var attatched_position : float = 0
 
 @export var detatchThreshold : float = 70.0
 
 func _physics_process(delta: float) -> void:
-	var movementinput := Input.get_axis("PlayerLeft","PlayerRight")
-	if attatchedrail!=null:
-		var lastAttachedInfo := attatchedrail.get_point_along_path(attatchedposition);
+	var movementInput := Input.get_axis("PlayerLeft","PlayerRight")
+	if attatched_rail!=null:
+		var lastMomentum := momentum;
+		var lastAttachedInfo := attatched_rail.get_point_along_path(attatched_position);
 		momentum.y = 0.0;
-		momentum += Vector2.RIGHT * movementinput * delta * movementspeed;
-		attatchedposition += momentum.x * delta;
+		momentum.x += movementInput * delta * movement_speed;
+		momentum.x += Vector2.RIGHT.dot(lastAttachedInfo.normal) * ProjectSettings.get_setting("physics/2d/default_gravity") * delta;
+		attatched_position += momentum.x * delta;
 		
-		var attachedInfo := attatchedrail.get_point_along_path(attatchedposition);
+		var attachedInfo := attatched_rail.get_point_along_path(attatched_position);
 		position = attachedInfo.position;
 		rotation = (PI / 2) + attachedInfo.normal.angle();
 		
 		if (Input.is_action_just_pressed("PlayerJump")):
-			attatchedrail = null;
 			momentum.y = -500.0;
+			detatch();
+		
+		#if (attachedInfo.normal.dot(Vector2.UP) <= 0.05 && 
+			#(sign(momentum.x) != sign(lastMomentum.x) || absf(momentum.x) <= 0.5)):
+			#detatch();
 		
 		var angle := rad_to_deg(lastAttachedInfo.normal.angle_to(attachedInfo.normal));
 		if ((momentum.x > 0.0 && angle >= detatchThreshold) || 
 			(momentum.x < 0.0 && angle <= -detatchThreshold)):
-			attatchedrail = null;
+			detatch();
 		elif ((momentum.x < 0.0 && angle >= detatchThreshold) || 
 			(momentum.x > 0.0 && angle <= -detatchThreshold)):
-			attatchedposition -= momentum.x * delta;
+			attatched_position -= momentum.x * delta;
 			momentum.x *= -0.45;
 			return _physics_process(0.0);
 		
-		if (attatchedrail == null):
+		if (attatched_rail == null):
 			momentum = momentum.rotated(-lastAttachedInfo.normal.angle_to(Vector2.UP))
 			return _physics_process(0.0);
 	else:
 		rotation = 0.0;
 		
-		momentum += Vector2.RIGHT * movementinput * delta * movementspeed;
+		momentum += Vector2.RIGHT * movementInput * delta * movement_speed;
 		momentum += Vector2.DOWN * ProjectSettings.get_setting("physics/2d/default_gravity") * delta;
 		
-		var closestrailinfo := WorldRails.get_closest_rail(position)
-		ImmediateGizmos2D.line_circle(closestrailinfo.closest_position, 30)
-		if (momentum.normalized().dot(position.direction_to(closestrailinfo.closest_position)) > 0.0 && 
-			closestrailinfo.closest_distance <= momentum.length() * delta):
-			attatchedrail = closestrailinfo.rail_path;
-			attatchedposition = closestrailinfo.rail_path_distance;
-			momentum = momentum.rotated(closestrailinfo.rail_normal.angle_to(Vector2.UP));
+		var closestRailInfo := WorldRails.get_closest_rail(position)
+		ImmediateGizmos2D.line_circle(closestRailInfo.closest_position, 30)
+		if (momentum.normalized().dot(position.direction_to(closestRailInfo.closest_position)) > 0.0 && 
+			closestRailInfo.closest_distance <= momentum.length() * delta):
+			attatched_rail = closestRailInfo.rail_path;
+			attatched_position = closestRailInfo.rail_path_distance;
+			momentum = momentum.rotated(closestRailInfo.rail_normal.angle_to(Vector2.UP));
 		else:
 			position += momentum * delta
 
 	momentum *= 0.99;
+	
+func detatch() -> void:
+	if (attatched_rail == null): return;
+	
+	attatched_rail = null;
