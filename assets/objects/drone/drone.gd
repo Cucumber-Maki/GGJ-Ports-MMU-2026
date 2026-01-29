b05a6654aa1055a20ce706f72c563fc9275a047d
jawdan-dev <34403286+jawdan-dev@users.noreply.github.com>
2026-01-29 23:50:44 +0000

    feat: add drone damage underneath

--- assets/objects/drone/drone.gd
+++ assets/objects/drone/drone.gd
@@ -2,11 +2,16 @@ extends Entity
 
 @export var force : float = 500.0;
 @export_range(0.0, 2.0) var horizontal : float = 1.0;
+@export var push_force := Vector2(150.0, 100.0);
 
 func _on_body_entered(body: Node2D) -> void:
 	var player := body as Player;
 	if (player == null): return;
 	
-	player.set_momentum(Vector2.UP.rotated(rotation) * force, true, Vector2(horizontal, 0.0));
-	player.set_color(spectrum_color);
-	explode();
+	if (player.position.y >= position.y + 10.0):
+		player.set_momentum(push_force, true);
+		player.take_damage();
+	else:
+		player.set_momentum(Vector2.UP.rotated(rotation) * force, true, Vector2(horizontal, 0.0));
+		player.set_color(spectrum_color);
+		explode();
