9e9d9ea52eb0c1233f791175e37bc2ca188d3573
jawdan-dev <34403286+jawdan-dev@users.noreply.github.com>
2026-01-29 17:55:09 +0000

    feat: add spring centering

--- assets/objects/spring/spring.gd
+++ assets/objects/spring/spring.gd
@@ -1,16 +1,20 @@
 extends Area2D
 
 @export var force : float = 700.0;
+@export_range(0.0, 2.0) var horizontal : float = 0.0;
+@export var center_player : bool = false;
 
 func _on_body_entered(body: Node2D) -> void:
 	var player := body as Player;
 	if (player == null): return;
 	
-	player.set_momentum(Vector2.UP.rotated(rotation) * force, true);
+	player.set_momentum(Vector2.UP.rotated(rotation) * force, true, Vector2(horizontal, 0.0));
+	if (center_player):
+		player.position = position + (Vector2.UP.rotated(rotation) * 36);
+	
 	$Sprite.play("Bounced");
 	$Sprite.frame = 0;
 	$Sprite.frame_progress = 0;
-
 
 func _on_sprite_animation_finished() -> void:
 	$Sprite.play("Idle")
